ab044de29a829fc5c51f2de5c131e1e3852a68eb
mihailgaberov <mihail.gaberov@gmail.com>
2025-01-18 15:37:27 +0100

    :goal_net: add ErrorBoundary

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import RefreshButton from "./components/RefreshButton";
 import Grid from "./components/Grid";
 import Loader from "./components/Loader";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   const [listings, setListings] = useLocalStorage("properties", []);
@@ -18,12 +19,16 @@ function App() {
     setListings([]);
 
     try {
-      const response = await axios.get("http://localhost:5001/scrape");
+      const END_POINT = import.meta.env.VITE_API_URL;
+      console.log(">>> endpoint: ", END_POINT);
+      const response = await axios.get(END_POINT);
+
       if (response.data.length === 0) {
         throw new Error("No listings found");
       }
       setListings(response.data);
     } catch (err) {
+      console.log(">>> err: ", err);
       setError(
         err.response?.data?.error ||
           "Failed to fetch listings. Please try again."
@@ -40,15 +45,17 @@ function App() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Header />
-      <RefreshButton callback={fetchListings} loading={loading} />
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-4 relative">
-        {error && <p className="text-red-500">{error}</p>}
-        {loading ? <Loader /> : <Grid listings={listings} />}
-      </main>
-      <Footer />
-    </div>
+    <ErrorBoundary>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <Header />
+        <RefreshButton callback={fetchListings} loading={loading} />
+        <main className="flex flex-col items-center justify-center flex-1 w-full px-4 relative">
+          {error && <p className="text-red-500">{error}</p>}
+          {loading ? <Loader /> : <Grid listings={listings} />}
+        </main>
+        <Footer />
+      </div>
+    </ErrorBoundary>
   );
 }
 
