e960e7de24b40923edb3df6b087a63dffca69343
mihailgaberov <mihail.gaberov@gmail.com>
2025-01-19 12:05:07 +0100

    :lipstick: adjust header alignment on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
   return (
     <header className="bg-blue-500 w-full py-4 text-white text-center">
-      <h1 className="text-3xl font-bold">Airbnb Listings</h1>
+      <h1 className="text-left p-4 md:p-0 md:text-center md:text-3xl font-bold">
+        Airbnb Listings
+      </h1>
     </header>
   );
 }
